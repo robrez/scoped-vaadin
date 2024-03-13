c4e7b62c52c3eb80baa30777d06fb937066a9a49
Rob Resendez <robrez@users.noreply.github.com>
2024-03-13 00:12:39 -0500

    Support/23.x part2 (#17)
    * Removed files from git
    * updated gitignore

--- packages/overlay/theme/lumo/vaadin-overlay-styles.js
+++
@@ -1,4 +0,0 @@
-import { overlay } from '@scoped-vaadin/vaadin-lumo-styles/mixins/overlay.js';
-import { registerStyles } from '@scoped-vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
-
-registerStyles('vaadin23-overlay', overlay, { moduleId: 'lumo-vaadin-overlay' });
--- packages/tooltip/theme/material/vaadin-tooltip.js
+++
@@ -1,3 +0,0 @@
-import '@scoped-vaadin/overlay/theme/material/vaadin-overlay.js';
-import './vaadin-tooltip-styles.js';
-import '../../src/vaadin-tooltip.js';
